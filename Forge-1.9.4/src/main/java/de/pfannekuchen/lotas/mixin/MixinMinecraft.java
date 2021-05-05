c39f6a5402073078e1c3f7f7c90c12bb96329992
Pfannekuchen <gamesupport@mgnet.work>
2021-05-05 15:04:55 +0200

    Reset Dupe Cashe when closing a World

--- Forge-1.9.4/src/main/java/de/pfannekuchen/lotas/mixin/MixinMinecraft.java
+++ Forge-1.9.4/src/main/java/de/pfannekuchen/lotas/mixin/MixinMinecraft.java
@@ -14,6 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import de.pfannekuchen.lotas.Binds;
 import de.pfannekuchen.lotas.challenges.ChallengeLoader;
 import de.pfannekuchen.lotas.config.ConfigManager;
+import de.pfannekuchen.lotas.dupemod.DupeMod;
 import de.pfannekuchen.lotas.gui.GuiChallengeEscape;
 import de.pfannekuchen.lotas.savestate.SavestateMod;
 import de.pfannekuchen.lotas.tickratechanger.TickrateChanger;
@@ -57,6 +58,13 @@ public class MixinMinecraft {
 	
 	@Shadow
 	private boolean isGamePaused;
+	
+	@Inject(at = @At("HEAD"), method = "stopIntegratedServer")
+    private static void injectstopIntegratedServer() {
+		DupeMod.items.clear();
+		DupeMod.trackedObjects.clear();
+		DupeMod.tileentities.clear();
+    }
 	
 	@Inject(method = "loadWorld", at = @At("HEAD"))
 	public void injectloadWorld(WorldClient worldClientIn, CallbackInfo ci) {
