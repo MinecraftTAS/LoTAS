597d076a8eecb1a86768229dbda0b87ab3ceede4
Scribble <ScribbleLP@users.noreply.github.com>
2021-06-16 22:51:15 +0200

    [Fabric 1.16.1] Upgrade: AIManipulationScreen

--- LoTAS-Fabric/src/main/java/de/pfannekuchen/lotas/gui/AIManipulationScreen.java
+++ LoTAS-Fabric/src/main/java/de/pfannekuchen/lotas/gui/AIManipulationScreen.java
@@ -9,6 +9,9 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.TextFieldWidget;
+//#if MC>=11601
+//$$ import net.minecraft.client.util.math.MatrixStack;
+//#endif
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.text.LiteralText;
 
@@ -24,11 +27,17 @@ public class AIManipulationScreen extends Screen {
 	public TextFieldWidget xText;
 	public TextFieldWidget yText;
 	public TextFieldWidget zText;
-
-	public static int spawnX = (int) MinecraftClient.getInstance().player.x;
-	public static int spawnY = (int) MinecraftClient.getInstance().player.y;
-	public static int spawnZ = (int) MinecraftClient.getInstance().player.z;
-
+	
+	//#if MC>=11601
+//$$ 	public static int spawnX = (int) MinecraftClient.getInstance().player.getX();
+//$$     public static int spawnY = (int) MinecraftClient.getInstance().player.getY();
+//$$     public static int spawnZ = (int) MinecraftClient.getInstance().player.getZ();
+	//#else
+	 public static int spawnX = (int) MinecraftClient.getInstance().player.x;
+    public static int spawnY = (int) MinecraftClient.getInstance().player.y;
+    public static int spawnZ = (int) MinecraftClient.getInstance().player.z;
+	//#endif
+	
 	@Override
 	public void init() {
 		addButton(new NewButtonWidget(5, 5, 98, 20, "<", btn -> {
@@ -41,11 +50,17 @@ public class AIManipulationScreen extends Screen {
 			button.active = selectedIndex != entities.size() - 1;
 			buttons.get(0).active = selectedIndex != 0;
 		}));
-
+		
+		//#if MC>=11601
+//$$ 		xText = new TextFieldWidget(MinecraftClient.getInstance().textRenderer, width / 2 - 100, height - 50, 60, 20, new LiteralText(spawnX + ""));
+//$$ 		yText = new TextFieldWidget(MinecraftClient.getInstance().textRenderer, width / 2 - 30, height - 50, 60, 20, new LiteralText(spawnY + ""));
+//$$ 		zText = new TextFieldWidget(MinecraftClient.getInstance().textRenderer, width / 2 + 40, height - 50, 60, 20, new LiteralText(spawnZ + ""));
+		//#else
 		xText = new TextFieldWidget(MinecraftClient.getInstance().textRenderer, width / 2 - 100, height - 50, 60, 20, spawnX + "");
-		yText = new TextFieldWidget(MinecraftClient.getInstance().textRenderer, width / 2 - 30, height - 50, 60, 20, spawnY + "");
-		zText = new TextFieldWidget(MinecraftClient.getInstance().textRenderer, width / 2 + 40, height - 50, 60, 20, spawnZ + "");
-
+        yText = new TextFieldWidget(MinecraftClient.getInstance().textRenderer, width / 2 - 30, height - 50, 60, 20, spawnY + "");
+        zText = new TextFieldWidget(MinecraftClient.getInstance().textRenderer, width / 2 + 40, height - 50, 60, 20, spawnZ + "");
+		//#endif
+		
 		addButton(new NewButtonWidget(width / 2 - 100, height - 25, 200, 20, "Change Target", button -> {
 			button.active = !entities.get(selectedIndex).getNavigation().startMovingTo(spawnX, spawnY, spawnZ, 1.0f);
 		}));
@@ -57,18 +72,30 @@ public class AIManipulationScreen extends Screen {
 		addButton(new NewButtonWidget(width / 2 + 40, height - 72, 60, 20, "Z++", btn -> spawnZ++));
 		addButton(new NewButtonWidget(width / 2 + 40, height - 94, 60, 20, "Z--", btn -> spawnZ--));
 		addButton(new NewButtonWidget(width / 2 - 100, height - 116, 200, 20, "Move to me", btn -> {
+		    //#if MC>=11601
+//$$ 		    MinecraftClient.getInstance().player.getX();
+//$$ 		    MinecraftClient.getInstance().player.getY();
+//$$ 		    MinecraftClient.getInstance().player.getZ();
+		    //#else
 			spawnX = (int) minecraft.player.x;
 			spawnY = (int) minecraft.player.y;
 			spawnZ = (int) minecraft.player.z;
+			//#endif
 			xText.setText(spawnX + "");
 			yText.setText(spawnY + "");
 			zText.setText(spawnZ + "");
 		}));
 		addButton(new NewButtonWidget(width / 2 - 100, height - 138, 200, 20, "Move to entity", btn -> {
 			try {
+			    //#if MC>=11601
+//$$ 			    spawnX = (int) entities.get(selectedIndex).getX();  
+//$$ 			    spawnY = (int) entities.get(selectedIndex).getY();  
+//$$ 			    spawnZ = (int) entities.get(selectedIndex).getZ();  
+			    //#else
 				spawnX = (int) entities.get(selectedIndex).x;
 				spawnY = (int) entities.get(selectedIndex).y;
 				spawnZ = (int) entities.get(selectedIndex).z;
+				//#endif
 				xText.setText(spawnX + "");
 				yText.setText(spawnY + "");
 				zText.setText(spawnZ + "");
@@ -76,7 +103,11 @@ public class AIManipulationScreen extends Screen {
 				e1.printStackTrace();
 			}
 		}));
+		//#if MC>=11601
+//$$ 		entities = MinecraftClient.getInstance().getServer().getPlayerManager().getPlayerList().get(0).getServerWorld().getEntities(MobEntity.class, MinecraftClient.getInstance().player.getBoundingBox().expand(32, 32, 32), Predicates.alwaysTrue());
+		//#else
 		entities = minecraft.getServer().getWorld(MinecraftClient.getInstance().player.dimension).getEntities(MobEntity.class, minecraft.player.getBoundingBox().expand(32, 32, 32), Predicates.alwaysTrue());
+		//#endif
 		selectedIndex = 0;
 
 		if (selectedIndex + 2 > entities.size()) {
@@ -154,7 +185,17 @@ public class AIManipulationScreen extends Screen {
 		}
 		return super.charTyped(typedChar, keyCode);
 	}
-
+	//#if MC>=11601
+//$$ 	@Override
+//$$ 	public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+//$$ 		super.render(matrices, mouseX, mouseY, delta);
+//$$ 		if (entities.size() == 0) return;
+//$$ 		xText.render(matrices, mouseX, mouseY, delta);
+//$$ 		yText.render(matrices, mouseX, mouseY, delta);
+//$$ 		zText.render(matrices, mouseX, mouseY, delta);
+//$$ 		drawCenteredString(matrices, MinecraftClient.getInstance().textRenderer, entities.get(selectedIndex).getClass().getSimpleName().replaceFirst("Entity", "") + " (" + entities.get(selectedIndex).getX() + ", " + entities.get(selectedIndex).getY() + ", " + entities.get(selectedIndex).getZ() + ")", width / 2, 5, 0xFFFFFF);
+//$$ 	}
+	//#else
 	@Override
 	public void render(int mouseX, int mouseY, float partialTicks) {
 		super.render(mouseX, mouseY, partialTicks);
@@ -165,5 +206,5 @@ public class AIManipulationScreen extends Screen {
 		zText.render(mouseX, mouseY, partialTicks);
 		drawCenteredString(MinecraftClient.getInstance().textRenderer, entities.get(selectedIndex).getClass().getSimpleName().replaceFirst("Entity", "") + " (" + entities.get(selectedIndex).x + ", " + entities.get(selectedIndex).y + ", " + entities.get(selectedIndex).z + ")", width / 2, 5, 0xFFFFFF);
 	}
-
+	//#endif
 }
