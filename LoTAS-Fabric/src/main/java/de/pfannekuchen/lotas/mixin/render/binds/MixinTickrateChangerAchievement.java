d42a23b76aa4ad0f9c2ad847908957c1ab038147
Scribble <ScribbleLP@users.noreply.github.com>
2021-06-18 20:26:28 +0200

    [Fabric] Fixing advancements again

--- LoTAS-Fabric/src/main/java/de/pfannekuchen/lotas/mixin/render/binds/MixinTickrateChangerAchievement.java
+++ LoTAS-Fabric/src/main/java/de/pfannekuchen/lotas/mixin/render/binds/MixinTickrateChangerAchievement.java
@@ -9,12 +9,12 @@ import de.pfannekuchen.lotas.mods.TickrateChangerMod;
 @Mixin(targets = "net/minecraft/client/toast/ToastManager$Entry")
 public abstract class MixinTickrateChangerAchievement {
 	//#if MC>=11601
-//$$ 	@ModifyVariable(method = "draw", at = @At(value = "STORE"), ordinal = 0, index = 4)
+//$$ 	@ModifyVariable(method = "Lnet/minecraft/client/toast/ToastManager;draw(IILnet/minecraft/client/util/math/MatrixStack;)Z", at = @At(value = "STORE"), ordinal = 0, index = 4)
 //$$ 	public long modifyAnimationTime(long animationTimer) {
 //$$ 		return TickrateChangerMod.getMilliseconds();
 //$$ 	}
 	//#else
-	@ModifyVariable(method = "draw", at = @At(value = "STORE"), ordinal = 0, index = 3)
+	@ModifyVariable(method = "Lnet/minecraft/client/toast/ToastManager;draw(II)Z", at = @At(value = "STORE"), ordinal = 0, index = 3)
 	public long modifyAnimationTime(long animationTimer) {
 		return TickrateChangerMod.getMilliseconds();
 	}
