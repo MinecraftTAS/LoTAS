01bdd17a768a521138ff015c9877c2b6092e34e5
Pfannekuchen <gamesupport@mgnet.work>
2021-06-18 21:44:15 +0200

    [Forge] Fix being dum dum

--- LoTAS-Forge/src/main/java/de/pfannekuchen/lotas/core/utils/PotionRenderingUtils.java
+++ LoTAS-Forge/src/main/java/de/pfannekuchen/lotas/core/utils/PotionRenderingUtils.java
@@ -8,7 +8,7 @@ import net.minecraft.item.ItemPotion;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 //#if MC<=10809
-//$$ import net.minecraft.util.math.MathHelper;
+//$$ import net.minecraft.util.MathHelper;
 //$$ import net.minecraft.init.Items;
 //#endif
 
