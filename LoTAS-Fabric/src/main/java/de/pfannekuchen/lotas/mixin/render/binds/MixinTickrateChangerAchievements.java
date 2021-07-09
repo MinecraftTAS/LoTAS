dad4b8336f73e843e123ee1b034d6cead9308eb0
MCPfannkuchenYT <gamesupport@mgnet.work>
2021-07-09 21:33:04 +0200

    [Fabric] Bind the Toasts to the Tickrate

---
+++ LoTAS-Fabric/src/main/java/de/pfannekuchen/lotas/mixin/render/binds/MixinTickrateChangerAchievements.java
@@ -0,0 +1,24 @@
+package de.pfannekuchen.lotas.mixin.render.binds;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import de.pfannekuchen.lotas.mods.TickrateChangerMod;
+
+@Mixin(targets = "net/minecraft/client/gui/components/toasts/ToastComponent$ToastInstance")
+public class MixinTickrateChangerAchievements {
+
+	//#if MC>=11601
+	//$$ 	@ModifyVariable(method = "draw", at = @At(value = "STORE"), ordinal = 0, index = 4)
+	//$$ 	public long modifyAnimationTime(long animationTimer) {
+	//$$ 		return TickrateChangerMod.getMilliseconds();
+	//$$ 	}
+	//#else
+	@ModifyVariable(method = "render", at = @At(value = "STORE"), ordinal = 0, index = 3)
+	public long modifyAnimationTime(long animationTimer) {
+		return TickrateChangerMod.getMilliseconds();
+	}
+	//#endif
+
+}
