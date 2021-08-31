37bf1efa35f0e980242bce0d728e02647f56491d
famous1622 <8428080+famous1622@users.noreply.github.com>
2021-08-31 12:40:38 -0400

    Remove throwable inaccuracy

---
+++ LoTAS-Forge/src/main/java/de/pfannekuchen/lotas/mixin/patches/MixinInaccuracyPatch.java
@@ -0,0 +1,32 @@
+package de.pfannekuchen.lotas.mixin.patches;
+
+import de.pfannekuchen.lotas.core.utils.ConfigUtils;
+import net.minecraft.entity.projectile.EntityThrowable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.Random;
+
+/**
+ * Allows for removing the inaccuracy from throwables
+ * (is theoretically possible if the stars align on 3 gaussian randoms)
+ *
+ * @author CittyKat
+ */
+@Mixin(EntityThrowable.class)
+public abstract class MixinInaccuracyPatch {
+    //#if MC>=11200
+    @Redirect(method = "shoot(DDDFF)V", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextGaussian()D", remap = false))
+    //#else
+//$$ 	@Redirect(method = "setThrowableHeading", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextGaussian()D", remap = false))
+    //#endif
+    private double redirect_internalShoot(Random random) {
+        if (ConfigUtils.getBoolean("tools", "removeThrowableInaccuracy")) {
+            return 0;
+        } else {
+            return random.nextGaussian();
+        }
+    }
+}
