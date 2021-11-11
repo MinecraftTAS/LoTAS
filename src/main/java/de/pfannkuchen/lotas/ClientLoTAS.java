b13eef792b3697e4e4d52cf4673f066fce47f9b6
MCPfannkuchenYT <gamesupport@mgnet.work>
2021-11-11 22:15:38 +0100

    [WIP] Introducing LoScreen™️

--- src/main/java/de/pfannkuchen/lotas/ClientLoTAS.java
+++ src/main/java/de/pfannkuchen/lotas/ClientLoTAS.java
@@ -1,5 +1,7 @@
 package de.pfannkuchen.lotas;
 
+import de.pfannkuchen.lotas.gui.api.LoScreenRenderer;
+import de.pfannkuchen.lotas.gui.api.MainLoScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -14,10 +16,13 @@ public class ClientLoTAS implements ClientModInitializer {
 
 	// Client-side LoTAS Singleton
 	public static ClientLoTAS instance;
+	// LoScreen Renderer Singleton
+	public static LoScreenRenderer loscreenrenderer;
 	
 	@Override
 	public void onInitializeClient() {
 		ClientLoTAS.instance = this;
+		ClientLoTAS.loscreenrenderer = new LoScreenRenderer();
 	}
 
 	/**
@@ -25,7 +30,11 @@ public class ClientLoTAS implements ClientModInitializer {
 	 * @param mc Instance of Minecraft
 	 */
 	public void onRenderInitialize(Minecraft mc) {
-		
+		// Initialize LoScreen
+		loscreenrenderer.onGameInitialize(mc);
+		// EXAMPLE CODE!!
+		loscreenrenderer.setScreen(new MainLoScreen());
+		// END
 	}
 	
 	/**
@@ -49,7 +58,26 @@ public class ClientLoTAS implements ClientModInitializer {
 	 * @param mc Instance of Minecraft
 	 */
 	public void onGameLoop(Minecraft mc) {
-		
+		// Update LoScreen
+		loscreenrenderer.onGameLoop(mc);
+	}
+
+	/**
+	 * Executed after the gui screens are rendered. Only executed while in a world
+	 * @param mc Instance of Minecraft
+	 */
+	public void onRenderScreen(Minecraft mc) {
+		// Render LoScreen
+		loscreenrenderer.onGuiRender(mc);
+	}
+	
+	/**
+	 * Executed after the gui screens are rendered. Only executed while in a world
+	 * @param mc Instance of Minecraft
+	 */
+	public void onDisplayResize(Minecraft mc) {
+		// Resize LoScreen
+		loscreenrenderer.onDisplayResize(mc);
 	}
 	
 }
---
+++ src/main/java/de/pfannkuchen/lotas/gui/api/LoScreenRenderer.java
@@ -0,0 +1,97 @@
+package de.pfannkuchen.lotas.gui.api;
+
+import org.jetbrains.annotations.Nullable;
+
+import com.mojang.blaze3d.platform.Window;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.Minecraft;
+
+/**
+ * Renders and manages the LoScreens.
+ * @author Pancake
+ */
+@Environment(EnvType.CLIENT)
+public class LoScreenRenderer {
+	
+	// Currently drawn LoScreen
+	private LoScreen screen;
+	// Shared Minecraft Instance
+	private Minecraft mc;
+	// Last resolution in case of resizing
+	private double lastWidth;
+	private double lastHeight;
+	// Last state of mouse buttons
+	private boolean wasLeftPressed;
+	private boolean wasMiddlePressed;
+	private boolean wasRightPressed;
+	// Last mouse position
+	private double lastPosX;
+	private double lastPosY;
+	
+	/**
+	 * Updates the current LoScreen and adds the Minecraft Instance. Can be null.
+	 * @param screen Update LoScreen
+	 */
+	public void setScreen(@Nullable LoScreen screen) {
+		this.screen = screen;
+		if (screen != null) {
+			screen.mc = this.mc;
+			screen.update(lastWidth, lastHeight);
+		}
+	}
+	
+	/**
+	 * Updates the Screen.
+	 * @param mc Instance of Minecraft
+	 */
+	public void onGameLoop(Minecraft mc) {
+		// Update Inputs
+		final double posX = mc.mouseHandler.xpos();
+		final double posY = mc.mouseHandler.ypos();
+		final boolean isLeftPressed = mc.mouseHandler.isLeftPressed();
+		final boolean isMiddlePressed = mc.mouseHandler.isMiddlePressed();
+		final boolean isRightPressed = mc.mouseHandler.isRightPressed();
+		// Check whether clicks/moves have passed.
+		if (screen != null) {
+			if (!isLeftPressed && this.wasLeftPressed) this.screen.click(posX, posY, 0);
+			if (!isMiddlePressed && this.wasMiddlePressed) this.screen.click(posX, posY, 2);
+			if (!isRightPressed && this.wasRightPressed) this.screen.click(posX, posY, 1);
+			if (isLeftPressed || isRightPressed || isMiddlePressed) this.screen.drag(this.lastPosX, this.lastPosY, posX, posY);
+		}
+		this.wasLeftPressed = isLeftPressed;
+		this.wasMiddlePressed = isMiddlePressed;
+		this.wasRightPressed = isRightPressed;
+		this.lastPosX = posX;
+		this.lastPosY = posY;
+	}
+
+	/**
+	 * Updates the Minecraft Instance once the game launches
+	 * @param mc Instance of Minecraft
+	 */
+	public void onGameInitialize(Minecraft mc) {
+		this.mc = mc;
+	}
+	
+	/**
+	 * Renders the next LoScreen
+	 * @param mc Instance of Minecraft
+	 */
+	public void onGuiRender(Minecraft mc) {
+		if (this.screen != null) this.screen.render(lastPosX, lastPosY);
+	}
+
+	/**
+	 * Resizes the LoScreen.
+	 * @param mc Instance of Minecraft
+	 */
+	public void onDisplayResize(Minecraft mc) {
+		final Window w = mc.getWindow();
+		this.lastWidth = w.getWidth();
+		this.lastHeight = w.getHeight();
+		if (this.screen != null) this.screen.update(this.lastWidth, this.lastHeight);
+	}
+	
+}
