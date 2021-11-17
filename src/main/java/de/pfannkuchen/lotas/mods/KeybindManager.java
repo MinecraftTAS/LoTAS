a4f9ea949e3affd48dc144ae69d0ee36cbc396b3
MCPfannkuchenYT <gamesupport@mgnet.work>
2021-11-17 19:17:13 +0100

    Finish Keybind Manager and add a hotkey for opening the MainLoScreen

--- src/main/java/de/pfannkuchen/lotas/mods/KeybindManager.java
+++ src/main/java/de/pfannkuchen/lotas/mods/KeybindManager.java
@@ -1,14 +1,17 @@
 package de.pfannkuchen.lotas.mods;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.lang3.ArrayUtils;
 import org.lwjgl.glfw.GLFW;
 
+import de.pfannkuchen.lotas.ClientLoTAS;
 import de.pfannkuchen.lotas.mixin.client.MixinKeyMappingsAccessor;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
 
 /**
  * Manages keybinds and their categories.
@@ -23,7 +26,7 @@ public class KeybindManager {
 	private KeyMapping openLoTASMenuKeybind = new KeyMapping("key.openlotasmenu", GLFW.GLFW_KEY_R, "key.categories.lotas");
 	
 	/**
-	 * Categories for all keybinds used
+	 * Categories for all keybinds used.
 	 */
 	private String[] keybindCategories = new String[] {"key.categories.lotas"};
 	
@@ -36,6 +39,33 @@ public class KeybindManager {
 		for (int i = 0; i < keybindCategories.length; i++) categories.put(keybindCategories[i], i+8);
 		// Finish by adding Keybinds
 		return ArrayUtils.addAll(keyMappings, openLoTASMenuKeybind);
+	}
+	
+	/**
+	 * Watches out for key presses and triggers sub events.
+	 * @param mc Instance of Minecraft
+	 */
+	public void onGameLoop(Minecraft mc) {
+		if (isKeyDown(mc, openLoTASMenuKeybind))
+			ClientLoTAS.instance.toggleLoTASMenu(mc);
+	}
+	
+	/**
+	 * Map of pressed/unpressed Keys.
+	 */
+	private Map<KeyMapping, Boolean> keys = new HashMap<>();
+	
+	/**
+	 * Checks whether a key has been pressed recently.
+	 * @param mc Instance of Minecraft
+	 * @param map Key Mappings to check
+	 * @return Key has been pressed recently
+	 */
+	private boolean isKeyDown(Minecraft mc, KeyMapping map) {
+		boolean wasPressed = keys.containsKey(map) ? keys.get(map) : false;
+		boolean isPressed = GLFW.glfwGetKey(mc.getWindow().getWindow(), ((MixinKeyMappingsAccessor) openLoTASMenuKeybind).getKey().getValue()) == GLFW.GLFW_PRESS;
+		keys.put(map, isPressed);
+		return !wasPressed && isPressed;
 	}
 	
 }
