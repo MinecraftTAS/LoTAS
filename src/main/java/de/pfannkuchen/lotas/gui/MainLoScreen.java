8af5351485c97228037b921bfbd9eead699132a7
MCPfannkuchenYT <gamesupport@mgnet.work>
2021-11-17 15:15:09 +0100

    [WIP] Change Visibility Modifiers for LoScreen™

--- src/main/java/de/pfannkuchen/lotas/gui/MainLoScreen.java
+++ src/main/java/de/pfannkuchen/lotas/gui/MainLoScreen.java
@@ -14,7 +14,7 @@ import de.pfannkuchen.lotas.gui.widgets.MainLoWidget;
 public class MainLoScreen extends LoScreen {
 	
 	@Override
-	public void init() {
+	protected void init() {
 		addWidget(new MainLoWidget(0.25, 0.005, 0.075, 0xFF1c1a1e, 0xFF025a5f, 0xFF038386, 0xFFFFFFFF, "LoTAS Menu"));
 	}
 	
