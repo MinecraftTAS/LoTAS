03384de439cf50adaa6c89545230c7b58f0ab6e6
MCPfannkuchenYT <gamesupport@mgnet.work>
2021-11-17 15:04:05 +0100

    [WIP] Add LoScreen™ Widgets

--- src/main/java/de/pfannkuchen/lotas/gui/api/LoScreen.java
+++ src/main/java/de/pfannkuchen/lotas/gui/api/LoScreen.java
@@ -1,5 +1,8 @@
 package de.pfannkuchen.lotas.gui.api;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import com.mojang.blaze3d.vertex.PoseStack;
 
 import net.fabricmc.api.EnvType;
@@ -12,11 +15,70 @@ import net.minecraft.client.Minecraft;
  */
 @Environment(EnvType.CLIENT)
 public abstract class LoScreen {
-
+	
+	/**
+	 * Accessable Minecraft Instance.
+	 */
 	public Minecraft mc;
-	public abstract void update(double width, double height);
-	public abstract void render(PoseStack stack, double curX, double curY);
-	public abstract void drag(double prevCurX, double prevCurY, double curX, double curY);
-	public abstract void click(double curX, double curY, int button);
+	
+	/**
+	 * List of Widgets managed by the LoScreen itself.
+	 */
+	public List<LoScreen> widgets = new LinkedList<>();
+	
+	/**
+	 * Adds a Widget and updates its minecraft variable
+	 * @param widget Widget to add
+	 */
+	public final void addWidget(LoScreen widget) {
+		this.widgets.add(widget);
+		widget.mc = this.mc;
+		widget.init();
+	}
+	
+	/**
+	 * Initializes the Gui and it's widgets.
+	 */
+	public abstract void init();
+	
+	/**
+	 * Updates the Gui and it's widgets.
+	 * @param width Scaled Width of the Minecraft screen
+	 * @param height Scaled Height of the Minecraft screen
+	 */
+	public void update(double width, double height) {
+		widgets.forEach(w -> w.update(width, height));
+	}
+	
+	/**
+	 * Renders the Gui and it's widgets.
+	 * @param stack Pose Stack for rendering
+	 * @param curX Current cursor x position
+	 * @param curY Current cursor y position
+	 */
+	public void render(PoseStack stack, double curX, double curY) {
+		widgets.forEach(w -> w.render(stack, curX, curY));
+	}
+	
+	/**
+	 * Event for the cursor once it drags. Updates the Gui and it's widgets.
+	 * @param prevCurX Previous cursor x position
+	 * @param prevCurY Previous cursor y position
+	 * @param curX Current cursor x position
+	 * @param curY Current cursor y position
+	 */
+	public void drag(double prevCurX, double prevCurY, double curX, double curY) {
+		widgets.forEach(w -> w.drag(prevCurX, prevCurY, curX, curY));
+	}
+	
+	/**
+	 * Event for the cursor once it clicks. Updates the Gui and it's widgets.
+	 * @param curX Current cursor x position
+	 * @param curY Current cursor y position
+	 * @param button Button clicked with
+	 */
+	public void click(double curX, double curY, int button) {
+		widgets.forEach(w -> w.click(curX, curY, button));
+	}
 	
 }
---
+++ src/main/java/de/pfannkuchen/lotas/gui/widgets/MainLoWidget.java
@@ -0,0 +1,87 @@
+package de.pfannkuchen.lotas.gui.widgets;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+
+import de.pfannkuchen.lotas.gui.api.LoScreen;
+import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.network.chat.TextComponent;
+
+/*
+ * WARNING: EXAMPLE CODE
+ */
+
+/**
+ * This is the Main LoWidget that shows a list of items on the right. Currently it is only an example though.
+ * @author Pancake
+ */
+public class MainLoWidget extends LoScreen {
+
+	// Width of the right panel
+	private double widgetWidthRaw;
+	private int widgetWidth;
+	// Width of the left border
+	private double borderWidthRaw;
+	private int borderWidth;
+	// Height of title text
+	private double titleHeightRaw;
+	private int titleHeight;
+	// Total screen sizes
+	private int screenWidth;
+	private int screenHeight;
+	// Colors
+	private int backgroundColor;
+	private int borderColor;
+	private int focusedBorderColor;
+	private int titleColor;
+	// Text
+	private TextComponent titleText;
+	
+	/**
+	 * Initializes the right list with some parameters
+	 * @param widgetWidth Width of the Widget in .percentage
+	 * @param borderWidth Width of the left border in .percentage
+	 * @param titleHeight Height of the title in .percentage
+	 * @param backgroundColor Color of the background
+	 * @param borderColor Color of the border
+	 * @param focusedBorderColor Color of the border when focused
+	 * @param titleColor Color of the title text
+	 * @param titleText Title Text
+	 */
+	public MainLoWidget(double widgetWidth, double borderWidth, double titleHeight, int backgroundColor, int borderColor, int focusedBorderColor, int titleColor, String titleText) {
+		this.widgetWidthRaw = widgetWidth;
+		this.borderWidthRaw = borderWidth;
+		this.titleHeightRaw = titleHeight;
+		this.backgroundColor = backgroundColor;
+		this.borderColor = borderColor;
+		this.focusedBorderColor = focusedBorderColor;
+		this.titleColor = titleColor;
+		this.titleText = new TextComponent(titleText);
+	}
+	
+	@Override
+	public void update(double width, double height) {
+		// Update widget specific values
+		this.widgetWidth = (int) (width * this.widgetWidthRaw);
+		this.borderWidth = (int) (width * this.borderWidthRaw);
+		this.titleHeight = (int) (height * this.titleHeightRaw);
+		// Update screen sizes
+		this.screenWidth = (int) width;
+		this.screenHeight = (int) height;
+	}
+	
+	@Override
+	public void render(PoseStack stack, double curX, double curY) {
+		final boolean isMouseOver = curX > (this.screenWidth - this.widgetWidth);
+		// Render Background
+		GuiComponent.fill(stack, this.screenWidth - this.widgetWidth, 0, this.screenWidth, this.screenHeight, this.backgroundColor);
+		GuiComponent.fill(stack, this.screenWidth - this.widgetWidth - this.borderWidth, 0, this.screenWidth - this.widgetWidth, this.screenHeight, isMouseOver ? this.focusedBorderColor : this.borderColor);
+		// Render Text
+		GuiComponent.drawCenteredString(stack, this.mc.font, this.titleText, this.screenWidth - (this.widgetWidth / 2), this.titleHeight, this.titleColor);
+	}
+
+	@Override
+	public void init() {
+		
+	}
+	
+}
