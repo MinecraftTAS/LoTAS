4f40f7d9a3fba8e16c44a73434aac2a80c5af7a9
MCPfannkuchenYT <gamesupport@mgnet.work>
2021-11-17 16:11:19 +0100

    [WIP] Fixing categories for the Keybind Manager
    And format all files to have \t instead of '  '

---
+++ src/main/java/de/pfannkuchen/lotas/mixin/client/MixinKeyMappingsAccessor.java
@@ -0,0 +1,30 @@
+package de.pfannkuchen.lotas.mixin.client;
+
+import java.util.Map;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import de.pfannkuchen.lotas.mods.KeybindManager;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.KeyMapping;
+
+/**
+ * This mixin accessor makes the categories array for {@link KeybindManager} accessible.
+ * @author Pancake
+ */
+@Mixin(KeyMapping.class)
+@Environment(EnvType.CLIENT)
+public interface MixinKeyMappingsAccessor {
+
+	/**
+	 * This Accessor opens the static field containing all Categories with orders
+	 * @return CATEGORY_SORT_ORDER
+	 */
+	@Accessor("CATEGORY_SORT_ORDER")
+	public static Map<String, Integer> getCategorySorting() {
+		throw new AssertionError();
+	}
+	
+}
--- src/main/java/de/pfannkuchen/lotas/mods/KeybindManager.java
+++ src/main/java/de/pfannkuchen/lotas/mods/KeybindManager.java
@@ -1,25 +1,40 @@
 package de.pfannkuchen.lotas.mods;
 
+import java.util.Map;
+
 import org.apache.commons.lang3.ArrayUtils;
 import org.lwjgl.glfw.GLFW;
 
+import de.pfannkuchen.lotas.mixin.client.MixinKeyMappingsAccessor;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.KeyMapping;
 
 /**
- * Manages keybinds.
+ * Manages keybinds and their categories.
  * @author Pancake
  */
+@Environment(EnvType.CLIENT)
 public class KeybindManager {
 
 	/**
 	 * Keybinding for opening and closing the LoTAS Menu LoScreen.
 	 */
-	private KeyMapping openLoTASMenuKeybind = new KeyMapping("Open LoTAS Menu", GLFW.GLFW_KEY_R, "LoTAS");
+	private KeyMapping openLoTASMenuKeybind = new KeyMapping("Open LoTAS Menu", GLFW.GLFW_KEY_R, "key.categories.lotas");
 	
 	/**
-	 * Initializes the Keybind Manager, registers keybinds.
+	 * Categories for all keybinds used
+	 */
+	private String[] keybindCategories = new String[] {"key.categories.lotas"};
+	
+	/**
+	 * Initializes the Keybind Manager, registers categories and keybinds.
 	 */
 	public KeyMapping[] onKeybindInitialize(KeyMapping[] keyMappings) {
+		// Initialize Categories first
+		final Map<String, Integer> categories = MixinKeyMappingsAccessor.getCategorySorting();
+		for (int i = 0; i < keybindCategories.length; i++) categories.put(keybindCategories[i], i+8);
+		// Finish by adding Keybinds
 		return ArrayUtils.addAll(keyMappings, openLoTASMenuKeybind);
 	}
 	
