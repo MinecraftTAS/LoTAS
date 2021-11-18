16a8da0f5e6fa1b1cead10d29e25392a60958c1f
MCPfannkuchenYT <gamesupport@mgnet.work>
2021-11-18 20:52:35 +0100

    Finish Main Widget

--- src/main/java/de/pfannkuchen/lotas/gui/MainLoScreen.java
+++ src/main/java/de/pfannkuchen/lotas/gui/MainLoScreen.java
@@ -4,6 +4,7 @@ import de.pfannkuchen.lotas.gui.widgets.MainLoWidget;
 import de.pfannkuchen.lotas.loscreen.LoScreen;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.network.chat.TextComponent;
 
 /**
  * This is the Main LoScreen that opens on the LoTAS Edit button.
@@ -14,7 +15,11 @@ public class MainLoScreen extends LoScreen {
 	
 	@Override
 	protected void init() {
-		addWidget(new MainLoWidget());
+		addWidget(new MainLoWidget((a, b) -> {
+			if (a.getString().startsWith("\u00A7\u00A7\u00A7l")) a = new TextComponent(a.getString().replaceAll("\u00A7\u00A7\u00A7l", ""));
+			else a = new TextComponent("\u00A7\u00A7\u00A7l" + a.getString());
+			return a;
+		}));
 	}
 	
 }
