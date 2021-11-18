5d5d97698bd46f6c6ab0ce7d45bd5c6545e052e8
MCPfannkuchenYT <gamesupport@mgnet.work>
2021-11-18 16:04:31 +0100

    [WIP] Add Text to the Main Widget
    This took me 6 hours. I know

--- src/main/java/de/pfannkuchen/lotas/gui/widgets/MainLoWidget.java
+++ src/main/java/de/pfannkuchen/lotas/gui/widgets/MainLoWidget.java
@@ -24,6 +24,15 @@ public class MainLoWidget extends LoScreen {
 	// Height of title text
 	private double titleHeightRaw;
 	private int titleHeight;
+	// Height of category text
+	private double textHeightRaw;
+	private int textHeight;
+	// Height of gap inbetween category text
+	private double textGapRaw;
+	private int textGap;
+	// Height of bottom text
+	private double bottomHeightRaw;
+	private int bottomHeight;
 	// Total screen sizes
 	private int screenWidth;
 	private int screenHeight;
@@ -32,8 +41,13 @@ public class MainLoWidget extends LoScreen {
 	private int borderColor;
 	private int focusedBorderColor;
 	private int titleColor;
+	private int categoryColor;
+	private int bottomColor;
 	// Text
 	private TextComponent titleText;
+	private TextComponent[] categoryTexts;
+	private TextComponent versionText1;
+	private TextComponent versionText2;
 	
 	// Render Counter
 	private float animationProgress;
@@ -44,21 +58,38 @@ public class MainLoWidget extends LoScreen {
 	 * @param widgetWidth Width of the Widget in .percentage
 	 * @param borderWidth Width of the left border in .percentage
 	 * @param titleHeight Height of the title in .percentage
+	 * @param textHeight Height of the first Category text
+	 * @param textGap Gap between the category texts
+	 * @param bottomHeight Height for the bottom Text
 	 * @param backgroundColor Color of the background
 	 * @param borderColor Color of the border
 	 * @param focusedBorderColor Color of the border when focused
+	 * @param categoryColor Color of the category text
+	 * @param bottomColor Color of the version string
 	 * @param titleColor Color of the title text
 	 * @param titleText Title Text
+	 * @param versionText1 Higher Version Text
+	 * @param versionText2 Lower Version Text
+	 * @param categoryTexts Clickable texts
 	 */
-	public MainLoWidget(double widgetWidth, double borderWidth, double titleHeight, int backgroundColor, int borderColor, int focusedBorderColor, int titleColor, String titleText) {
+	public MainLoWidget(double widgetWidth, double borderWidth, double titleHeight, double textHeight, double textGap, double bottomHeight, int backgroundColor, int borderColor, int focusedBorderColor, int titleColor, int categoryColor, int bottomColor, String titleText, String versionText1, String versionText2, String... categoryTexts) {
 		this.widgetWidthRaw = widgetWidth;
 		this.borderWidthRaw = borderWidth;
 		this.titleHeightRaw = titleHeight;
+		this.textHeightRaw = textHeight;
+		this.textGapRaw = textGap;
+		this.bottomHeightRaw = bottomHeight;
 		this.backgroundColor = backgroundColor;
 		this.borderColor = borderColor;
 		this.focusedBorderColor = focusedBorderColor;
+		this.categoryColor = categoryColor;
+		this.bottomColor = bottomColor;
 		this.titleColor = titleColor;
 		this.titleText = new TextComponent(titleText);
+		this.categoryTexts = new TextComponent[categoryTexts.length];
+		this.versionText1 = new TextComponent(versionText1);
+		this.versionText2 = new TextComponent(versionText2);
+		for (int i = 0; i < categoryTexts.length; i++) this.categoryTexts[i] = new TextComponent(categoryTexts[i]);
 	}
 	
 	@Override
@@ -67,6 +98,9 @@ public class MainLoWidget extends LoScreen {
 		this.widgetWidth = (int) (width * this.widgetWidthRaw);
 		this.borderWidth = (int) (width * this.borderWidthRaw);
 		this.titleHeight = (int) (height * this.titleHeightRaw);
+		this.textHeight = (int) (height * this.textHeightRaw);
+		this.textGap = (int) (height * this.textGapRaw);
+		this.bottomHeight = (int) (height * this.bottomHeightRaw);
 		// Update screen sizes
 		this.screenWidth = (int) width;
 		this.screenHeight = (int) height;
@@ -86,7 +120,7 @@ public class MainLoWidget extends LoScreen {
 	
 	@Override
 	public void render(PoseStack stack, double curX, double curY) {
-		animationProgressInterpolated = Math.min(3, animationProgressInterpolated + mc.getDeltaFrameTime() / 4);
+		animationProgressInterpolated = Math.min(3, animationProgressInterpolated + mc.getDeltaFrameTime() / 2);
 		animationProgress = ease(animationProgressInterpolated, 0, 6, 3);
 		final boolean isMouseOver = curX > (this.screenWidth - this.widgetWidth);
 		// Render Background
@@ -94,9 +128,21 @@ public class MainLoWidget extends LoScreen {
 		GuiComponent.fill(stack, (int) (this.screenWidth - ((this.widgetWidth - this.borderWidth) * (animationProgress / 6))) - this.borderWidth, 0, (int) (this.screenWidth - ((this.widgetWidth - this.borderWidth) * (animationProgress / 6))), this.screenHeight, isMouseOver ? this.focusedBorderColor : this.borderColor);
 		// Render Text
 		stack.pushPose();
-		stack.scale(2f, 2f, 2f);
-		GuiComponent.drawCenteredString(stack, this.mc.font, this.titleText, (int) ((this.screenWidth - ((this.widgetWidth / 2) * (animationProgress / 6))) /2), this.titleHeight/2, this.titleColor);
+		float screenWidthFloat = Integer.valueOf(this.mc.getWindow().getGuiScaledWidth()).floatValue();
+		float scaleFactor = screenWidthFloat / 475.0f;
+		stack.translate(this.screenWidth - this.widgetWidth / 2 - this.mc.font.width(this.titleText)*scaleFactor/2, this.titleHeight, 0);
+		stack.scale(scaleFactor, scaleFactor, scaleFactor);
+		this.mc.font.draw(stack, this.titleText, 0, 0, this.titleColor);
 		stack.popPose();
+		// Render Categories
+		int categoryY = this.textHeight;
+		for (int i = 0; i < this.categoryTexts.length; i++) {
+			this.mc.font.draw(stack, this.categoryTexts[i], this.screenWidth - (this.widgetWidth * (animationProgress / 6)) + this.borderWidth*6, categoryY, this.categoryColor);
+			categoryY += this.textGap;
+		}
+		// Render Version String
+		this.mc.font.draw(stack, this.versionText1, this.screenWidth - (this.widgetWidth * (animationProgress / 6)) + this.borderWidth*3, bottomHeight, this.bottomColor);
+		this.mc.font.draw(stack, this.versionText2, this.screenWidth - (this.widgetWidth * (animationProgress / 6)) + this.borderWidth*3, (float) (bottomHeight + this.mc.font.lineHeight*1.3), this.bottomColor);
 	}
 
 	@Override
