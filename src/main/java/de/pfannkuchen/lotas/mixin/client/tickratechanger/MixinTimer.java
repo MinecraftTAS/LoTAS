1120cbf336f2bb8fc81644a0095afafe073f6b0b
Pfannekuchen <gamesupport@mgnet.work>
2022-01-07 16:42:45 +0100

    Better MixinTimer Implementation

--- src/main/java/de/pfannkuchen/lotas/mixin/client/tickratechanger/MixinTimer.java
+++ src/main/java/de/pfannkuchen/lotas/mixin/client/tickratechanger/MixinTimer.java
@@ -1,8 +1,12 @@
 package de.pfannkuchen.lotas.mixin.client.tickratechanger;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import de.pfannkuchen.lotas.LoTAS;
 import net.fabricmc.api.EnvType;
@@ -17,23 +21,12 @@ import net.minecraft.client.Timer;
 @Environment(EnvType.CLIENT)
 public class MixinTimer {
 
-	// FIXME: Mixin wouldn't allow ModifyVariable, so i had to do this override. 
+	@Shadow @Final @Mutable
+	public float msPerTick;
 	
-	@Shadow
-	public float partialTick;
-    @Shadow
-    public float tickDelta;
-    @Shadow
-    private long lastMs;
-
-    @Overwrite
-    public int advanceTime(long l) {
-        this.tickDelta = (float)(l - this.lastMs) / (float) ((LoTAS.tickadvance.isTickadvanceEnabled() && !LoTAS.tickadvance.shouldTick) ? Float.MAX_VALUE : LoTAS.tickratechanger.getMsPerTick());
-        this.lastMs = l;
-        this.partialTick += this.tickDelta;
-        int i = (int)this.partialTick;
-        this.partialTick -= (float)i;
-        return i;
-    }
+	@Inject(method = "advanceTime", at = @At("HEAD"))
+	public void onAdvanceTime(CallbackInfoReturnable<Integer> cir) {
+		this.msPerTick = (float) ((LoTAS.tickadvance.isTickadvanceEnabled() && !LoTAS.tickadvance.shouldTick) ? Float.MAX_VALUE : LoTAS.tickratechanger.getMsPerTick());
+	}
 	
 }
