12deb5acfbd5cae571fa2e308f215b689f323a95
MCPfannkuchenYT <gamesupport@mgnet.work>
2022-04-15 18:17:21 +0200

    Add keybinds for saving and loading the playerdata

--- src/main/java/de/pfannkuchen/lotas/mods/KeybindManager.java
+++ src/main/java/de/pfannkuchen/lotas/mods/KeybindManager.java
@@ -47,6 +47,16 @@ public class KeybindManager {
 	 */
 	private KeyMapping increaseTickrate = new KeyMapping("Increase Tickrate", GLFW.GLFW_KEY_PERIOD, "LoTAS Keybinds");
 	
+	/**
+	 * Key binding for increasing the tickrate
+	 */
+	private KeyMapping savePlayerdata = new KeyMapping("Save playerdata", GLFW.GLFW_KEY_N, "LoTAS Keybinds");
+	
+	/**
+	 * Key binding for decreasing the tickrate
+	 */
+	private KeyMapping loadPlayerdata = new KeyMapping("Load playerdata", GLFW.GLFW_KEY_M, "LoTAS Keybinds");
+	
 	private KeyMapping testingKey = new KeyMapping("Testing", GLFW.GLFW_KEY_F10, "LoTAS Keybinds");
 	private KeyMapping testingKey2 = new KeyMapping("Testing", GLFW.GLFW_KEY_F12, "LoTAS Keybinds");
 	private KeyMapping testingKey3 = new KeyMapping("Testing", GLFW.GLFW_KEY_J, "LoTAS Keybinds");
@@ -64,7 +74,7 @@ public class KeybindManager {
 		final Map<String, Integer> categories = AccessorKeyMapping.getCategorySorting();
 		for (int i = 0; i < this.keybindCategories.length; i++) categories.put(this.keybindCategories[i], i+8);
 		// Finish by adding Keybinds
-		return ArrayUtils.addAll(keyMappings, this.openLoTASMenuKeybind, this.tickadvanceKeybind, this.toggleTickadvance, this.decreaseTickrate, this.increaseTickrate);
+		return ArrayUtils.addAll(keyMappings, this.openLoTASMenuKeybind, this.tickadvanceKeybind, this.toggleTickadvance, this.decreaseTickrate, this.increaseTickrate, this.savePlayerdata, this.loadPlayerdata);
 	}
 
 	/**
@@ -82,6 +92,11 @@ public class KeybindManager {
 			LoTAS.tickratechanger.decreaseTickrate();
 		else if (this.isKeyDown(mc, this.increaseTickrate) && mc.level != null)
 			LoTAS.tickratechanger.increaseTickrate();
+		else if (this.isKeyDown(mc, this.savePlayerdata) && mc.level != null)
+			LoTAS.dupemod.requestDupe(true);
+		else if (this.isKeyDown(mc, this.loadPlayerdata) && mc.level != null)
+			LoTAS.dupemod.requestDupe(false);
+		
 		
 		else if (this.isKeyDown(mc, testingKey)) {
 			ChunkControl.unloadPlayer();
