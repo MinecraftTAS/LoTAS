ec722fdce99b77f14a52e0ec6eb5201d66fade7a
Pfannekuchen <gamesupport@mgnet.work>
2022-04-26 20:41:36 +0200

    Add Dragon Manipulation

---
+++ src/main/java/de/pfannkuchen/lotas/mixin/dragonmanipulation/RngmodDragonHoldingPatternPhase.java
@@ -0,0 +1,64 @@
+package de.pfannkuchen.lotas.mixin.dragonmanipulation;
+
+import java.util.Random;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import de.pfannkuchen.lotas.LoTAS;
+import net.minecraft.world.entity.boss.enderdragon.phases.DragonHoldingPatternPhase;
+
+/**
+ * This mixin manipulates the rng of the dragon holding pattern phase
+ * @author Pancake
+ */
+@Mixin(DragonHoldingPatternPhase.class)
+public class RngmodDragonHoldingPatternPhase {
+
+	/**
+	 * Manipulates the addend multiplier for determining the height of the next path node
+	 * @return Zero
+	 */
+	@Redirect(method = "navigateToNextPathNode", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextFloat()F"))
+	public float redirect_nextFloat(Random r) {
+		return LoTAS.configmanager.getBoolean("dragonmanipulationwidget", "forceOptimalPath") ? 0.999f : r.nextFloat();
+	}
+	
+	/**
+	 * Manipulates the landing approach phase integer
+	 * @return Zero
+	 */
+	@Redirect(method = "findNewTarget", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I", ordinal = 0))
+	public int redirect_nextInt_perching(Random r, int i) {
+		return LoTAS.configmanager.getBoolean("dragonmanipulationwidget", "forceLandingApproach") ? 0 : r.nextInt(i);
+	}
+	
+	/**
+	 * Manipulates the strafing phase integer
+	 * @return Zero
+	 */
+	@Redirect(method = "findNewTarget", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I", ordinal = 1))
+	public int redirect_nextInt_strafing(Random r, int i) {
+		return LoTAS.configmanager.getBoolean("dragonmanipulationwidget", "forcePlayerStrafing") ? 0 : r.nextInt(i);
+	}
+	
+	/**
+	 * Manipulates the strafing phase integer - PART 2
+	 * @return Zero
+	 */
+	@Redirect(method = "findNewTarget", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I", ordinal = 2))
+	public int redirect_nextInt_strafing2(Random r, int i) {
+		return LoTAS.configmanager.getBoolean("dragonmanipulationwidget", "forcePlayerStrafing") ? 0 : r.nextInt(i);
+	}
+	
+	/**
+	 * Manipulates the rotation changing integer
+	 * @return Zero
+	 */
+	@Redirect(method = "findNewTarget", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I", ordinal = 3))
+	public int redirect_nextInt(Random r, int i) {
+		return LoTAS.configmanager.getBoolean("dragonmanipulationwidget", "forceCCWToggle") ? 0 : r.nextInt(i);
+	}
+	
+}
---
+++ src/main/java/de/pfannkuchen/lotas/mods/DragonManipulationMod.java
@@ -0,0 +1,103 @@
+/**
+ * Here is the logic of the dragon manipulation mod:
+ *
+ * As noted by the @Environment annotations in front of methods, this code works on both client and server.
+ *
+ * Every time the clients wants to update the servers dragon manipulation states it sends a request #requestState which is handled in #onServerPacket and resends the packets, which 
+ * are being handled once again in #onClientPacket.
+ */
+package de.pfannkuchen.lotas.mods;
+
+import de.pfannkuchen.lotas.LoTAS;
+import de.pfannkuchen.lotas.gui.windows.DragonManipulationLoWidget;
+import io.netty.buffer.Unpooled;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.protocol.game.ClientboundCustomPayloadPacket;
+import net.minecraft.network.protocol.game.ServerboundCustomPayloadPacket;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
+
+/**
+ * Main dragon manipulation mod
+ * @author Pancake
+ */
+public class DragonManipulationMod {
+
+	static final ResourceLocation DRAGON_MANIPULATION_MOD_RL = new ResourceLocation("lotas", "dragonmanipulationwidget");
+	@Environment(EnvType.CLIENT)
+	public Minecraft mc;
+	public MinecraftServer mcserver;
+	
+	/**
+	 * Updates the local values on incoming packets
+	 * @param p Incoming packet
+	 */
+	@Environment(EnvType.CLIENT)
+	public void onClientPacket(ClientboundCustomPayloadPacket p) {
+		if (DRAGON_MANIPULATION_MOD_RL.equals(p.getIdentifier())) {
+			// Update Config Manager
+			FriendlyByteBuf data = p.getData();
+			LoTAS.configmanager.setBoolean("dragonmanipulationwidget", data.readUtf(), data.readBoolean());
+			LoTAS.configmanager.save();
+			DragonManipulationLoWidget.forceUpdate();
+		}
+	}
+
+	/**
+	 * Updates the variables and resends when receiving a packet
+	 * @param p Incoming packet
+	 */
+	public void onServerPacket(ServerboundCustomPayloadPacket p) {
+		if (DRAGON_MANIPULATION_MOD_RL.equals(p.getIdentifier())) {
+			// Update Config Manager
+			FriendlyByteBuf data = p.getData();
+			String key = data.readUtf();
+			boolean value = data.readBoolean();
+			LoTAS.configmanager.setBoolean("dragonmanipulationwidget", key, value);
+			LoTAS.configmanager.save();
+			// Resend packet to all clients
+			this.mcserver.getPlayerList().getPlayers().forEach(c -> {
+				FriendlyByteBuf buf = new FriendlyByteBuf(Unpooled.buffer());
+				buf.writeUtf(key);
+				buf.writeBoolean(value);
+				c.connection.send(new ClientboundCustomPayloadPacket(DRAGON_MANIPULATION_MOD_RL, buf));
+			});
+		}
+	}
+	
+	/**
+	 * Client-Side only state update request. Sends a packet to the server containg the state to be modified
+	 * @param key Key to change
+	 * @param value Value to change
+	 */
+	@Environment(EnvType.CLIENT)
+	public void requestState(String key, boolean value) {
+		FriendlyByteBuf buf = new FriendlyByteBuf(Unpooled.buffer());
+		buf.writeUtf(key);
+		buf.writeBoolean(value);
+		this.mc.getConnection().send(new ServerboundCustomPayloadPacket(DRAGON_MANIPULATION_MOD_RL, buf));
+	}
+	
+	/**
+	 * Updates client data on connect
+	 */
+	public void onConnect(ServerPlayer c) {
+		final String[] keys = new String[] {
+			"forceOptimalPath",
+			"forceCCWToggle",
+			"forceLandingApproach",
+			"forcePlayerStrafing"
+		};
+		for (int i = 0; i < keys.length; i++) {
+			FriendlyByteBuf buf = new FriendlyByteBuf(Unpooled.buffer());
+			buf.writeUtf(keys[i]);
+			buf.writeBoolean(LoTAS.configmanager.getBoolean("dragonmanipulationwidget", keys[i]));
+			c.connection.send(new ClientboundCustomPayloadPacket(DRAGON_MANIPULATION_MOD_RL, buf));
+		}
+	}
+	
+}
