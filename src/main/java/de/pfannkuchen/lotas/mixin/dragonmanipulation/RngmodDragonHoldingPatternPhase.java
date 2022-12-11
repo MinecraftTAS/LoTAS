cb89e0c956b7b3ed884f46e1690f7e4f8d00cd49
Pfannekuchen <gamesupport@mgnet.work>
2022-12-11 17:42:57 +0100

    Update to 1.19.3

--- src/main/java/de/pfannkuchen/lotas/mixin/dragonmanipulation/RngmodDragonHoldingPatternPhase.java
+++ src/main/java/de/pfannkuchen/lotas/mixin/dragonmanipulation/RngmodDragonHoldingPatternPhase.java
@@ -1,12 +1,11 @@
 package de.pfannkuchen.lotas.mixin.dragonmanipulation;
 
-import java.util.Random;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import de.pfannkuchen.lotas.LoTAS;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.boss.enderdragon.phases.DragonHoldingPatternPhase;
 
 /**
@@ -20,8 +19,8 @@ public class RngmodDragonHoldingPatternPhase {
 	 * Manipulates the addend multiplier for determining the height of the next path node
 	 * @return Zero
 	 */
-	@Redirect(method = "navigateToNextPathNode", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextFloat()F"))
-	public float redirect_nextFloat(Random r) {
+	@Redirect(method = "navigateToNextPathNode", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/RandomSource;nextFloat()F"))
+	public float redirect_nextFloat(RandomSource r) {
 		return LoTAS.configmanager.getBoolean("dragonmanipulationwidget", "forceOptimalPath") ? 0.999f : r.nextFloat();
 	}
 
@@ -29,8 +28,8 @@ public class RngmodDragonHoldingPatternPhase {
 	 * Manipulates the landing approach phase integer
 	 * @return Zero
 	 */
-	@Redirect(method = "findNewTarget", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I", ordinal = 0))
-	public int redirect_nextInt_perching(Random r, int i) {
+	@Redirect(method = "findNewTarget", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/RandomSource;nextInt(I)I", ordinal = 0))
+	public int redirect_nextInt_perching(RandomSource r, int i) {
 		return LoTAS.configmanager.getBoolean("dragonmanipulationwidget", "forceLandingApproach") ? 0 : r.nextInt(i);
 	}
 
@@ -38,8 +37,8 @@ public class RngmodDragonHoldingPatternPhase {
 	 * Manipulates the strafing phase integer
 	 * @return Zero
 	 */
-	@Redirect(method = "findNewTarget", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I", ordinal = 1))
-	public int redirect_nextInt_strafing(Random r, int i) {
+	@Redirect(method = "findNewTarget", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/RandomSource;nextInt(I)I", ordinal = 1))
+	public int redirect_nextInt_strafing(RandomSource r, int i) {
 		return LoTAS.configmanager.getBoolean("dragonmanipulationwidget", "forcePlayerStrafing") ? 0 : r.nextInt(i);
 	}
 
@@ -47,8 +46,8 @@ public class RngmodDragonHoldingPatternPhase {
 	 * Manipulates the strafing phase integer - PART 2
 	 * @return Zero
 	 */
-	@Redirect(method = "findNewTarget", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I", ordinal = 2))
-	public int redirect_nextInt_strafing2(Random r, int i) {
+	@Redirect(method = "findNewTarget", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/RandomSource;nextInt(I)I", ordinal = 2))
+	public int redirect_nextInt_strafing2(RandomSource r, int i) {
 		return LoTAS.configmanager.getBoolean("dragonmanipulationwidget", "forcePlayerStrafing") ? 0 : r.nextInt(i);
 	}
 
@@ -56,8 +55,8 @@ public class RngmodDragonHoldingPatternPhase {
 	 * Manipulates the rotation changing integer
 	 * @return Zero
 	 */
-	@Redirect(method = "findNewTarget", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I", ordinal = 3))
-	public int redirect_nextInt(Random r, int i) {
+	@Redirect(method = "findNewTarget", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/RandomSource;nextInt(I)I", ordinal = 3))
+	public int redirect_nextInt(RandomSource r, int i) {
 		return LoTAS.configmanager.getBoolean("dragonmanipulationwidget", "forceCCWToggle") ? 0 : r.nextInt(i);
 	}
 
--- src/main/java/de/pfannkuchen/lotas/mixin/dragonmanipulation/RngmodDragonStrafePlayerPhase.java
+++ src/main/java/de/pfannkuchen/lotas/mixin/dragonmanipulation/RngmodDragonStrafePlayerPhase.java
@@ -1,12 +1,11 @@
 package de.pfannkuchen.lotas.mixin.dragonmanipulation;
 
-import java.util.Random;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import de.pfannkuchen.lotas.LoTAS;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.boss.enderdragon.phases.DragonStrafePlayerPhase;
 
 /**
@@ -20,8 +19,8 @@ public class RngmodDragonStrafePlayerPhase {
 	 * Manipulates the addend multiplier for determining the height of the next path node
 	 * @return Zero
 	 */
-	@Redirect(method = "navigateToNextPathNode", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextFloat()F"))
-	public float redirect_nextFloat(Random r) {
+	@Redirect(method = "navigateToNextPathNode", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/RandomSource;nextFloat()F"))
+	public float redirect_nextFloat(RandomSource r) {
 		return LoTAS.configmanager.getBoolean("dupemodwidget", "forceOptimalPath") ? 0.999f : r.nextFloat();
 	}
 
@@ -29,8 +28,8 @@ public class RngmodDragonStrafePlayerPhase {
 	 * Manipulates the rotation changing integer
 	 * @return Zero
 	 */
-	@Redirect(method = "findNewTarget", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I"))
-	public int redirect_nextInt(Random r, int i) {
+	@Redirect(method = "findNewTarget", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/RandomSource;nextInt(I)I"))
+	public int redirect_nextInt(RandomSource r, int i) {
 		return LoTAS.configmanager.getBoolean("dupemodwidget", "forceCCWToggle") ? 0 : r.nextInt(i);
 	}
 
