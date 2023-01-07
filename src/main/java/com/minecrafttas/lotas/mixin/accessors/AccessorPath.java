b97cf5a890f92edf11d82d996c874f3b54943a51
Pfannekuchen <gamesupport@mgnet.work>
2023-01-08 00:08:13 +0100

    🚊🚋🚋🚋

--- src/main/java/com/minecrafttas/lotas/mixin/accessors/AccessorPath.java
+++ src/main/java/com/minecrafttas/lotas/mixin/accessors/AccessorPath.java
@@ -8,9 +8,17 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 import net.minecraft.world.level.pathfinder.Node;
 import net.minecraft.world.level.pathfinder.Path;
 
+/**
+ * This mixin accessor makes the nodes accessible for every version. Getters were removed in 1.16.5, why mojang???
+ * @author Pancake
+ */
 @Mixin(Path.class)
 public interface AccessorPath {
 
+	/**
+	 * This Accessor opens the private field containing the nodes for of the path
+	 * @return Nodes
+	 */
 	@Accessor("nodes")
 	public List<Node> nodes();
 	
