fc0529c054e98d9fc250500d05b41cd1b2177fa8
Pfannekuchen <gamesupport@mgnet.work>
2023-01-24 14:36:16 +0100

    Beginning to work on loot manipulation

---
+++ src/main/java/com/minecrafttas/lotas/mixin/accessors/AccessorLootTable.java
@@ -0,0 +1,23 @@
+package com.minecrafttas.lotas.mixin.accessors;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+
+/**
+ * This mixin accessor makes the loot pools accessible.
+ * @author Pancake
+ */
+@Mixin(LootTable.class)
+public interface AccessorLootTable {
+
+	/**
+	 * This Accessor opens the private field containing the list of loot tables
+	 * @return Identifier
+	 */
+	@Accessor("pools")
+	public LootPool[] pools();
+
+}
