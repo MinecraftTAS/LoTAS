34f184a5d710b20fb0341b4b362b42745bdb16d9
Pfannekuchen <gamesupport@mgnet.work>
2023-01-29 22:08:24 +0100

    More torture with loot manipulation

---
+++ src/main/java/com/minecrafttas/lotas/mixin/accessors/AccessorTagEntry.java
@@ -0,0 +1,24 @@
+package com.minecrafttas.lotas.mixin.accessors;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import net.minecraft.tags.Tag;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.storage.loot.entries.TagEntry;
+
+/**
+ * This mixin accessor makes the loot item in a tag entry accessible.
+ * @author Pancake
+ */
+@Mixin(TagEntry.class)
+public interface AccessorTagEntry {
+
+	/**
+	 * This Accessor opens the private field containing the list of tags
+	 * @return Tag
+	 */
+	@Accessor("tag")
+	public Tag<Item> tag();
+
+}
