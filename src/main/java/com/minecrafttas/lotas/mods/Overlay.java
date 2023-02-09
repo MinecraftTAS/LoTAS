9221b1e67f83e0fbac491893fa16f4947dd91b6f
Pfannekuchen <gamesupport@mgnet.work>
2023-02-09 13:50:48 +0100

    Add tickrate storing between worlds

---
+++ src/main/java/com/minecrafttas/lotas/mods/Overlay.java
@@ -0,0 +1,16 @@
+package com.minecrafttas.lotas.mods;
+
+import com.minecrafttas.lotas.system.ModSystem.Mod;
+
+import net.minecraft.resources.ResourceLocation;
+
+public class Overlay extends Mod {
+
+	/**
+	 * Initializes the overlay
+	 */
+	public Overlay() {
+		super(new ResourceLocation("lotas", "overlay"));
+	}
+
+}
