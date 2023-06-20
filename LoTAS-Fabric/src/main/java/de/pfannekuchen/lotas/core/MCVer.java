fd0f093167515ec8faceda21cbf7825feeaeeec3
Scribble <scribble@minecrafttas.com>
2023-06-20 22:26:11 +0200

    Fixed client crashing in 1.20

--- LoTAS-Fabric/src/main/java/de/pfannekuchen/lotas/core/MCVer.java
+++ LoTAS-Fabric/src/main/java/de/pfannekuchen/lotas/core/MCVer.java
@@ -302,12 +302,20 @@ public class MCVer {
 //$$ 	}
 //$$
 //$$ 	public static void translated(Object stack, double x, double y, double z) {
+		//#if MC>=12000
+//$$ 		com.mojang.blaze3d.vertex.PoseStack poseStack = ((net.minecraft.client.gui.GuiGraphics)stack).pose();
+		//#else
 //$$ 		com.mojang.blaze3d.vertex.PoseStack poseStack = (com.mojang.blaze3d.vertex.PoseStack)stack;
+		//#endif
 //$$ 		poseStack.translate(x, y, z);
 //$$ 	}
 //$$
 //$$ 	public static void scaled(Object stack, double scale, double scale2, double scale3) {
+		//#if MC>=12000
+//$$ 		com.mojang.blaze3d.vertex.PoseStack poseStack = ((net.minecraft.client.gui.GuiGraphics)stack).pose();
+		//#else
 //$$ 		com.mojang.blaze3d.vertex.PoseStack poseStack = (com.mojang.blaze3d.vertex.PoseStack)stack;
+		//#endif
 //$$ 		poseStack.scale((float)scale, (float)scale2, (float)scale3);
 //$$ 	}
 //$$
